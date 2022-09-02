5d28f7e0e9fb322c51e81f27250653af5bf7f81c
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2022-09-02 12:32:47 +0530

    dark mode added

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const dataNumber = document.querySelectorAll(".data-number");
 const clear = document.querySelector(".data-clear");
 const equal = document.querySelector(".data-equal");
 const clearOne = document.querySelector(".data-clearOne");
+const themeButton = document.querySelectorAll(".theme");
+const sun = document.querySelector(".sun");
+const moon = document.querySelector(".moon");
+const table = document.querySelector(".table");
+const red = document.querySelectorAll(".red");
+const body = document.querySelector(".body");
+const header = document.querySelector(".header");
 
 let displayValue = "";
 
@@ -33,3 +40,19 @@ equal.addEventListener("click", () => {
     alert("PLEASE INPUT VALID EXPRESSION");
   }
 });
+for (item of themeButton) {
+  item.addEventListener("click", () => {
+    sun.classList.toggle("hidden");
+    moon.classList.toggle("hidden");
+    for (button of buttons) {
+      button.classList.toggle("dark-mode-button");
+    }
+    display.classList.toggle("dark-mode-display");
+    table.classList.toggle("dark-mode-table");
+    for (item of red) {
+      item.classList.toggle("dark-mode-red");
+    }
+    body.classList.toggle("dark-mode-body");
+    header.classList.toggle("dark-mode-header");
+  });
+}
